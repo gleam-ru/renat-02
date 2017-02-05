b27c8109debed78d035d02fa906a0958a7207cd3
gleam <nobody.roman@gmail.com>
2017-02-05 19:00:42 +0300

    try-05

--- statify.bat
+++ statify.bat
@@ -1,1 +1,1 @@
-D:\Downloads\gleam\Dropbox\_BATs\wget64.exe --no-host-directories --mirror -r --html-extension --no-host-directories --no-parent -P C:\Users\gleam\work\renat-02\static\ http://localhost:1234/
+D:\Downloads\gleam\Dropbox\_BATs\wget64.exe --no-host-directories --base=https://gleam-ru.github.io/renat-02/static/ --mirror -r --html-extension --no-host-directories --no-parent -P C:\Users\gleam\work\renat-02\static\ http://localhost:1234/
